907616245c16e7062bba2804144ecccef2cd1388
Yourloverboy <1712555325@qq.com>
2018-11-15 18:53:29 +0800

    Site updated: 2018-11-15 18:53:27

--- js/src/snow.js
+++ js/src/snow.js
@@ -1,4 +1,5 @@
 "use strict";
+//圆形雪花
 var snow = function() {
 	var b = document.getElementById("christmasCanvas"), 
 		a = b.getContext("2d"), d = window.innerWidth, 
@@ -11,7 +12,7 @@ var snow = function() {
 	var h = 0;
 	window.intervral4Christmas = setInterval(function() {
 		a.clearRect(0, 0, d, c);
-		a.fillStyle = "rgba(255, 255, 0, 0.6)";
+		a.fillStyle = "rgba(254, 255, 0, 0.6)";
 		a.shadowBlur = 5;
 		a.shadowColor = "rgba(255, 255, 255, 0.9)";
 		a.beginPath();
@@ -19,14 +20,14 @@ var snow = function() {
  		var f = e[b];
  		a.moveTo(f.x, f.y);
  		a.arc(f.x, f.y, f.r, 0, Math.PI * 2, !0)
-	}
-	a.fill();
-	h += 0.01;
-	for(b = 0;b < 50;b++) {
-		if(f = e[b], f.y += Math.cos(h + f.d) + 1 + f.r / 2, f.x += Math.sin(h) * 2, f.x > d + 5 || f.x < -5 || f.y > c) {
-			e[b] = b % 3 > 0 ? {x:Math.random() * d, y:-10, r:f.r, d:f.d} : Math.sin(h) > 0 ? {x:-5, y:Math.random() * c, r:f.r, d:f.d} : {x:d + 5, y:Math.random() * c, r:f.r, d:f.d}
 		}
-	}
-}, 70)
+		a.fill();
+		h += 0.01;
+		for(b = 0;b < 50;b++) {
+			if(f = e[b], f.y += Math.cos(h + f.d) + 1 + f.r / 2, f.x += Math.sin(h) * 2, f.x > d + 5 || f.x < -5 || f.y > c) {
+				e[b] = b % 3 > 0 ? {x:Math.random() * d, y:-10, r:f.r, d:f.d} : Math.sin(h) > 0 ? {x:-5, y:Math.random() * c, r:f.r, d:f.d} : {x:d + 5, y:Math.random() * c, r:f.r, d:f.d}
+			}
+		}
+	}, 50)
 }
 snow();
