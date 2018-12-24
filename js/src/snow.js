09e2f438ff5862a29f60432992fce120a0b10198
Yourloverboy <1712555325@qq.com>
2018-12-24 12:15:24 +0800

    Site updated: 2018-12-24 12:15:22

--- js/src/snow.js
+++ js/src/snow.js
@@ -12,9 +12,9 @@ var snow = function() {
 	var h = 0;
 	window.intervral4Christmas = setInterval(function() {
 		a.clearRect(0, 0, d, c);
-		a.fillStyle = "rgba(255, 255, 0, 0.6)";
+		a.fillStyle = "rgba(255, 200, 0, 0.4)";
 		a.shadowBlur = 5;
-		a.shadowColor = "rgba(255, 255, 0, 0.5)";
+		a.shadowColor = "rgba(255, 200, 0, 0.5)";
 		a.beginPath();
 		for(var b = 0;b < 50;b++) {
  		var f = e[b];
